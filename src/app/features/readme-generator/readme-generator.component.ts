91371dbac4e86cccb10a2776154dcc4c3c714078
TreciaKS <82657928+TreciaKS@users.noreply.github.com>
2025-10-21 20:45:04 +0200

    refactor: read me

--- src/app/features/readme-generator/readme-generator.component.ts
+++ src/app/features/readme-generator/readme-generator.component.ts
@@ -6,7 +6,7 @@ interface ReadmeData {
   name: string;
   description: string;
   install: string;
-  usage: string;
+  features: string;
 }
 
 @Component({
@@ -16,30 +16,42 @@ interface ReadmeData {
   templateUrl: './readme-generator.component.html',
 })
 export class ReadmeGeneratorComponent {
-  data: ReadmeData = { name: '', description: '', install: '', usage: '' };
+  data: ReadmeData = { name: '', description: '', install: '', features: '' };
   markdown = '';
+  copyMessage = '';
 
   generate(): void {
     this.markdown = `# ${this.data.name || 'Project Title'}
 
-${this.data.description || 'Short description.'}
+    ${this.data.description || 'Short description.'}
 
-## Install
-\`\`\`bash
-${this.data.install || 'npm install'}
-\`\`\`
+    ## Features
+    \`\`\`The following features are found in # ${
+      this.data.name || 'the project:'
+    }
+    ${this.data.features || 'Can login without auth?'}
+    \`\`\`
 
-## Usage
-\`\`\`bash
-${this.data.usage || 'npm start'}
-\`\`\`
+    ## Installations
+    \`\`\`bash
+    ${this.data.install || 'Packages installed or dependencies'}
+    \`\`\`
 
-## License
-MIT
-`;
+    ## License
+    MIT
+    `;
   }
 
   copy(): void {
-    navigator.clipboard.writeText(this.markdown).catch(() => {});
+    navigator.clipboard
+      .writeText(this.markdown)
+      .then(() => {
+        this.copyMessage = '✅ Copied to clipboard!';
+        setTimeout(() => (this.copyMessage = ''), 2000);
+      })
+      .catch(() => {
+        this.copyMessage = '❌ Failed to copy';
+        setTimeout(() => (this.copyMessage = ''), 2000);
+      });
   }
 }
