94b0b10226c2c031bfec3974b6e1b10a469709c4
TreciaKS <82657928+TreciaKS@users.noreply.github.com>
2025-10-21 20:53:47 +0200

    del: removed globak search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { SearchBarComponent } from './shared/components/search-bar/search-bar.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, SearchBarComponent],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
