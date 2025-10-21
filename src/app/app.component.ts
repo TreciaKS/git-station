5fa5fc46b595f82bf86d1107942412b3f4dc87f4
TreciaKS <82657928+TreciaKS@users.noreply.github.com>
2025-10-21 21:34:33 +0200

    del: removed lottiefiles unused usings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { LottieComponent, AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, LottieComponent],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   appName = 'GitStation';
-  options: AnimationOptions = {
-    path: 'https://assets10.lottiefiles.com/packages/lf20_5ngs2ksb.json',
-  };
 }
